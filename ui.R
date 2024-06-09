b62f2e2bfd647e18221d565d29f9fdacd222d209
MakiniMakini <brianmakini@gmail.com>
2024-06-09 20:39:52 +0300

    updated flexboard for landing page

--- ui.R
+++ ui.R
@@ -19,45 +19,60 @@ bs4DashPage(
   ),
   sidebar = bs4DashSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("MAP", tabName = "map", icon = icon("dashboard")),
+      menuItem("Dashboard", tabName = "map", icon = icon("dashboard")),
+      menuItem("Table", tabName = "dashboard", icon = icon("dashboard")),
       menuItem("Settings", tabName = "settings", icon = icon("sliders"),
                menuSubItem("Subitem 1", tabName = "subitem1"),
                menuSubItem("Subitem 2", tabName = "subitem2"))
     )
   )
   ,
+  #body of the dashboard work out
   body = bs4DashBody(
-    tabItems(
-      tabItem(tabName = "dashboard",
-              DTOutput("table")
-      ),
+    tabItem(tabName = "map",
+            box(
+              title = "MAP-AMR Kenya",
+              solidHeader = TRUE,
+              status = "primary",
+              collapsible = TRUE,
+              width = 12,
+              fluidRow(
+                column(
+                  width = 12,
+                  fluidRow(
+                    column(
+                      width = 8,
+                      bs4Card(
+                        title = "MAP-AMR Kenya Sites",
+                        maximizable = TRUE,
+                        leafletOutput("map"),
+                        width = 12
+                      )
+                    ),
+                    column(
+                      width = 4,
+                      bs4Card(
+                        title = "Summary of Candida",
+                        maximizable = TRUE,
+                        plotlyOutput("piechart"),
+                        width = 12
+                      )
+                    )
+                  )
+                )
+              )
+            )
+    )
+    ,
       tabItem(tabName = "settings",
               box(status = "info", title = "Settings Box", solidHeader = TRUE)
       ),
-      tabItem(tabName = "map",
-              fluidRow(
-                column(width = 12,
-                       bs4Card(
-                         title = "Maximizable Map",
-                         maximizable = TRUE,
-                         leafletOutput("map")
-                       )
-                )
-              ),
-              fluidRow(
-                column(width = 8,
-                       bs4Card(
-                         title = "Pie Chart",
-                         maximizable = TRUE,
-                         plotlyOutput("piechart")
-                       )
-                )
-              )
-      )
-      
+  tabItem(
+    tabItem(tabName = "dashboard",
+            DTOutput("table"))
     )
-  ),
+  )
+  ,
   controlbar = bs4DashControlbar(
     skin = "orange",
     status = "primary",
